bb03d2355ec26550647bf8889cfdf012e8b6ad81
chayes <cfhayes16@gmail.com>
2021-02-12 23:54:28 -0700

    Adding blank line

--- src/__init__.py
+++ src/__init__.py
@@ -25,6 +25,7 @@ app.config.from_object(app_settings)
 # instantiate the db
 db = SQLAlchemy(app)
 
+
 class User(db.Model):
     __tablename__ = 'users'
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
