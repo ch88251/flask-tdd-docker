69caceaf3d69db71528a00f6d544cdcf5e4eaab3
chayes <cfhayes16@gmail.com>
2021-02-12 22:58:19 -0700

    completed docker config

--- src/__init__.py
+++ src/__init__.py
@@ -1,10 +1,15 @@
+# -*- coding: utf-8 -*-
+# pylint: disable=no-member
 """
 Created by Charles F. Hayes
 Date Created: Feb 12, 2021
 No rights reserved. Use as you wish.
 """
+import os
+
 from flask import Flask, jsonify
 from flask_restx import Resource, Api
+from flask_sqlalchemy import SQLAlchemy
 
 
 # instantiate the app
@@ -12,8 +17,28 @@ app = Flask(__name__)
 
 api = Api(app)
 
+
 # set config
-app.config.from_object('src.config.DevelopmentConfig')
+app_settings = os.getenv('APP_SETTINGS')
+app.config.from_object(app_settings)
+
+# instantiate the db
+db = SQLAlchemy(app)
+
+class User(db.Model):
+    __tablename__ = 'users'
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    username = db.Column(db.String(128), nullable=False)
+    first_name = db.Column(db.String(128), nullable=False)
+    last_name = db.Column(db.String(128), nullable=False)
+    email = db.Column(db.String(128), nullable=False)
+    active = db.Column(db.Boolean(), default=True, nullable=False)
+
+    def __init__(self, username, first_name, last_name, email):
+        self.username = username
+        self.first_name = first_name
+        self.last_name = last_name
+        self.email = email
 
 
 class Health(Resource):
--- src/config.py
+++ src/config.py
@@ -3,12 +3,15 @@ Created by Charles F. Hayes
 Date Created: Feb 12, 2021
 No rights reserved. Use as you wish.
 """
+import os
+
+
 class BaseConfig:
     """
     Configurations that are common for all environments
     """
     TESTING = False
-    DEBUG = False
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
 
 
 class DevelopmentConfig(BaseConfig):
@@ -16,15 +19,19 @@ class DevelopmentConfig(BaseConfig):
     Configurations that are specific for the development environment
     """
     DEBUG = True
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
+
 
 class TestingConfig(BaseConfig):
     """
     Configurations that are specific for the testing environment
     """
     TESTING = True
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_TEST_URL')
 
 
 class ProductionConfig(BaseConfig):
     """
     Configurations that are specific for the production environment
     """
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_PROD_URL')
